c2d6a0fc1b6fe0b27f3f3a0a8b68fa3b9ec4f075
Gennady Denisov <denisovgena@gmail.com>
2026-03-30 08:26:09 +0300

    (stopping_rules_relative_inexact_fw): add demonstration on regularized MSE problem

--- stopping_rules_fw_generalized_convexity/experiments.py
+++ stopping_rules_fw_generalized_convexity/experiments.py
@@ -2,13 +2,14 @@ from dataclasses import dataclass
 from typing import Callable
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 from common.algorithms import BaseAlgorithm
 from common.experiment_utils import title
 from common.latex_utils import latex_table
-from common.lmo import LMO, SimplexLMO
-from common.math_utils import non_singular_matrix, significant_figures
+from common.lmo import LMO, MinLinearDirectionL2BallLMO, SimplexLMO, LinfBallLMO
+from common.math_utils import non_singular_matrix, significant_figures, ensure_non_zero
 from common.objectives import Objective, MSE, LogisticRegression
 from common.regularization import L1Regular
 from common.plot_utils import preamble, do_show_plot
@@ -20,7 +21,7 @@ rng = np.random.default_rng(74)
 
 
 _TOLERANCE = 1e-6
-_ITERATIONS_COUNT = 5_000
+_ITERATIONS_COUNT = 2_000
 
 
 @dataclass
@@ -59,7 +60,7 @@ def setup_experiments(verbose: bool, _: bool) -> list[ExperimentData]:
             x0=x0,
         ),
     )
-    L0 = 1e-12
+    L0 = ensure_non_zero(0)
     L1 = np.linalg.norm(A, axis=1).max()
     log(f"{L0=}, {L1=}", verbose=verbose)
     experiments.append(
@@ -113,7 +114,7 @@ def _run_delta_iterations(
 ) -> None:
     log(title("Delta iterations"), verbose=verbose)
 
-    deltas = np.linspace(1e-12, 0.0005, 100)
+    deltas = np.linspace(ensure_non_zero(0), 0.0005, 100)
 
     plt.figure(figsize=(12, 6), dpi=100)
 
@@ -135,29 +136,37 @@ def _run_delta_iterations(
     do_show_plot(filename="denisov2.pgf", show_plot=True, interactive=interactive)
 
 
-def _run_l1_regularization_mse(
+def _run_l1_regularization_mse_linf_ball(
     experiments: list[ExperimentData],
     verbose: bool,
     interactive: bool,
 ) -> None:
     log(title("L1 regularization MSE Delta - iterations"), verbose=verbose)
 
-    n: int = 250  # problem dimension
+    n: int = 510  # problem dimension
 
-    A = rng.standard_normal((n, n)).astype(np.float64)
-    b = rng.random(n).astype(np.float64)
+    scale_factor = 0.03
+    A = scale_factor * non_singular_matrix(n, 0.2, 1.0, -1.0, 1.0, rng).astype(
+        np.float64
+    )
+    b = 0.25 * np.ones(n, dtype=np.float64)
 
-    x0 = np.zeros(n)
-    lam = 1e-1 * np.linalg.norm(np.dot(A, b))
-
+    center = 1.0
+    x0 = -np.ones(n, dtype=np.float64)
+    lam = ensure_non_zero(1e-6)
     obj = L1Regular(MSE(A, b), lam=lam)
 
-    # TODO(geaden): Remove duplication.
-    experiments: list[ExperimentData] = []
+    experiments = []
 
-    lmo = SimplexLMO()
     L = np.linalg.norm(A, axis=1).max() ** 2
-    log(f"{L=}", verbose=verbose)
+
+    L0 = ensure_non_zero(0)
+    L1 = np.linalg.norm(A, axis=1).max()
+
+    log(f"{L=}, {L0=}, {L1=}", verbose=verbose)
+
+    lmo = LinfBallLMO(radius=1.0, center=center)
+
     experiments.append(
         ExperimentData(
             label="Classic FW",
@@ -172,9 +181,6 @@ def _run_l1_regularization_mse(
             x0=x0,
         ),
     )
-    L0 = 1e-12
-    L1 = np.linalg.norm(A, axis=1).max()
-    log(f"{L0=}, {L1=}", verbose=verbose)
     experiments.append(
         ExperimentData(
             label=r"$(L_0, L_1)$-FW",
@@ -188,10 +194,10 @@ def _run_l1_regularization_mse(
                 tol=delta,
             ),
             x0=x0,
-        )
+        ),
     )
 
-    deltas = np.linspace(1e-12, 0.0005, 100)
+    deltas = np.linspace(ensure_non_zero(0), 2e-4, 100)
 
     plt.figure(figsize=(12, 6), dpi=100)
 
@@ -208,7 +214,8 @@ def _run_l1_regularization_mse(
     plt.xlabel(r"$\Delta$")
     plt.ylabel(r"$k$")
     plt.legend()
-    plot_title = obj.__doc__ + rf", $\lambda={lam:.4f}$"
+
+    plot_title = obj.__doc__ + rf", {n=}, {lmo}"
     log(plot_title, verbose=verbose)
     plt.title(plot_title)
     plt.grid()
@@ -278,7 +285,7 @@ def _run_l1_regularization_logreg(
         )
     )
 
-    deltas = np.linspace(1e-12, 0.0005, 100)
+    deltas = np.linspace(ensure_non_zero(0), 0.0005, 100)
 
     plt.figure(figsize=(12, 6), dpi=100)
 
@@ -295,7 +302,7 @@ def _run_l1_regularization_logreg(
     plt.xlabel(r"$\Delta$")
     plt.ylabel(r"$k$")
     plt.legend()
-    plot_title = obj.__doc__ + rf", $\lambda={lam:.6f}$"
+    plot_title = obj.__doc__
     log(plot_title, verbose=verbose)
     plt.title(plot_title)
     plt.grid()
@@ -310,10 +317,15 @@ def run_experiments(verbose: bool, interactive: bool):
 
     if not interactive:
         preamble()
+    else:
+        try:
+            matplotlib.use("TkAgg")
+        except:
+            log("TkAgg failed", verbose=verbose)
 
     _run_convegence_rate_stopping_rule(experiments, verbose, interactive)
     _run_delta_iterations(experiments, verbose, interactive)
-    _run_l1_regularization_mse(experiments, verbose, interactive)
     _run_l1_regularization_logreg(experiments, verbose, interactive)
+    _run_l1_regularization_mse_linf_ball(experiments, verbose, interactive)
 
     log("Done.", verbose=verbose)
